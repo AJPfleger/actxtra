4af2f7c84dc63bd196e63309e5301164009097b0
AJPfleger <alexander.julian.pfleger@cern.ch>
2023-01-27 06:31:29 +0100

    refactor

--- GX2F/chi2-2dimensional-yk-toydetector.py
+++ GX2F/chi2-2dimensional-yk-toydetector.py
@@ -14,7 +14,6 @@ from scipy.stats import norm
 import chi2_utilities as c2u
 
 
-#checked
 def straight_line_propagator(params, x_vec):
     y_vec = np.ones_like(x_vec) * params[0] + x_vec * np.ones_like(x_vec) * params[1]
 
@@ -22,31 +21,25 @@ def straight_line_propagator(params, x_vec):
 
 
 def get_pulls(plot_all, layers=12, cov=0.1):
-    
+
     ## Initialising
     n_update = 15
 
     # Parameter
     start_params = np.array([0.0, 3])  # [y, k]
     delta_params = np.zeros_like(start_params)
-    # updatedCov = np.array([
-    #     [3,0],
-    #     [0,0.1],
-    #     ])
 
     # Geometry
     # detectorLayers = np.array([2, 3, 5, 5.5, 5.7, 6.5])  # ,10,50,98,99,100])
     detectorLayers = np.random.uniform(1, 12, layers)
-    
+
     # Hits
     true_params = [12.345, 1]
     # true_params = [np.random.uniform(-9,9), np.random.uniform(-0.9,0.9)]
     measurments, cov_meas, measurments_raw = c2u.generate_hits(
         detectorLayers, true_params, straight_line_propagator, cov, True
     )
-    # proj = np.array([[1,0]]) # projects onto x
 
-    # start_params = true_params
     updated_params = start_params
 
     if plot_all:
@@ -67,12 +60,12 @@ def get_pulls(plot_all, layers=12, cov=0.1):
 
             ax.plot(0, params[0], "x" + color)
             ax.plot(traj[0], traj[1], color + style, label=label_text)
-    
+
     ## Iterating and updating parameters
     for _ in range(n_update):
 
         updated_params = updated_params + delta_params
-        
+
         # Iterate over surfaces
         a = np.zeros([2, 2])
         b = np.zeros_like(start_params)
@@ -83,13 +76,8 @@ def get_pulls(plot_all, layers=12, cov=0.1):
             Vi = cov_meas[d]
             ri = measurments[d] - straight_line_propagator(updated_params, h)
             chi2sum += c2u.chi2_1D(Vi, ri)
-            
-            ai = (
-                1 / Vi * np.array([
-                        [1,      h],
-                        [h, h**2],
-                        ])
-            )
+
+            ai = 1 / Vi * np.array([[1, h], [h, h ** 2],])
             bi = ri / Vi * np.array([1, h])
 
             a += ai
@@ -149,8 +137,6 @@ for d in range(draws):
     k_pulls.append(k_p)
 
 
-
-
 mu, std = norm.fit(y_pulls)
 fig, ax = plt.subplots()
 ax.hist(y_pulls, bins=bins, density=True)
@@ -163,7 +149,6 @@ fig.savefig("yk-y_pulls.png")
 plt.show()
 
 
-
 mu, std = norm.fit(k_pulls)
 fig, ax = plt.subplots()
 ax.hist(k_pulls, bins=bins, density=True)
@@ -174,4 +159,3 @@ ax.plot(x, p, "k")
 ax.set(title=f"k_pulls: mu = {mu:.5f}, std = {std:.5f}")
 fig.savefig("yk-k_pulls.png")
 plt.show()
-
--- GX2F/chi2_utilities.py
+++ GX2F/chi2_utilities.py
@@ -1,1 +1,21 @@
-import numpy as npdef generate_hits(geometry, true_params, propagator, cov=0.1, smearing=True):    measurments_raw = propagator(true_params, geometry)    cov_meas = [cov] * len(measurments_raw)    measurments = []    for mi in range(len(measurments_raw)):        if smearing:            m = np.random.normal(measurments_raw[mi], np.sqrt(cov_meas[mi]))        else:            m = measurments_raw[mi]        measurments.append(m)    return measurments, cov_meas, measurments_rawdef chi2_1D(V, r):    return r**2/V # r * (1 / V) * r
+import numpy as np
+
+
+def generate_hits(geometry, true_params, propagator, cov=0.1, smearing=True):
+    measurments_raw = propagator(true_params, geometry)
+
+    cov_meas = [cov] * len(measurments_raw)
+
+    measurments = []
+    for mi in range(len(measurments_raw)):
+        if smearing:
+            m = np.random.normal(measurments_raw[mi], np.sqrt(cov_meas[mi]))
+        else:
+            m = measurments_raw[mi]
+        measurments.append(m)
+
+    return measurments, cov_meas, measurments_raw
+
+
+def chi2_1D(V, r):
+    return r ** 2 / V  # r * (1 / V) * r
