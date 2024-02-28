6e17ef53487e35295570e6c5cf0d6dac61408486
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-02-28 10:43:46 +0100

    add mean and rms to plots

--- scripts/itk_full_plots.py
+++ scripts/itk_full_plots.py
@@ -3,6 +3,7 @@
 import uproot
 import matplotlib.pyplot as plt
 import awkward as ak
+import numpy as np
 import hist
 
 
@@ -26,11 +27,11 @@ n_bins_eta = 11
 
 # Interesting leaves
 all_leaves = (
-    "res_eLOC0_ubs",
-    "res_eLOC1_ubs",
-    "res_ePHI_ubs",
-    "res_eTHETA_ubs",
-    "res_eQOP_ubs",
+    # "res_eLOC0_ubs",
+    # "res_eLOC1_ubs",
+    # "res_ePHI_ubs",
+    # "res_eTHETA_ubs",
+    # "res_eQOP_ubs",
     # "res_eT_ubs",
     # "err_eLOC0_ubs",
     # "err_eLOC1_ubs",
@@ -38,11 +39,11 @@ all_leaves = (
     # "err_eTHETA_ubs",
     # "err_eQOP_ubs",
     # "err_eT_ubs",
-    # "pull_eLOC0_ubs",
-    # "pull_eLOC1_ubs",
-    # "pull_ePHI_ubs",
-    # "pull_eTHETA_ubs",
-    # "pull_eQOP_ubs",
+    "pull_eLOC0_ubs",
+    "pull_eLOC1_ubs",
+    "pull_ePHI_ubs",
+    "pull_eTHETA_ubs",
+    "pull_eQOP_ubs",
     # "pull_eT_ubs",
 )
 
@@ -59,12 +60,10 @@ all_hists = []
 for leave in all_leaves:
     data_leaf = df[leave]
 
-    h_min = min(ak.flatten(data_leaf))
-    h_max = max(ak.flatten(data_leaf))
-    # h_min = -6
-    # h_max = 6
-    # h_min = -6
-    # h_max = 6
+    # h_min = min(ak.flatten(data_leaf))
+    # h_max = max(ak.flatten(data_leaf))
+    h_min = -6
+    h_max = 6
 
     h = hist.Hist(
         hist.axis.Regular(bins=n_bins_leaf, start=h_min, stop=h_max, name=leave),
@@ -103,18 +102,23 @@ for df in tree.iterate(library="ak", how=dict):
         # all_hists[leave_i].plot2d(ax=ax, label=f"{leave}", ls="solid")
         # plt.show()
 
+# # fill with random for faster test
+# rand_size = 1_000_000
+# data_eta = np.random.normal(size=rand_size) * 2
+# volume_id = np.random.randint(20, size=rand_size)
+# layer_id = np.random.randint(6, size=rand_size)
+#
+# for leave_i, leave in enumerate(all_leaves):
+#     data_leaf = np.random.normal(size=rand_size) + np.random.normal(size=1)
+#
+#     all_hists[leave_i].fill(
+#         data_leaf,
+#         data_eta,
+#         volume_id,
+#         layer_id,
+#     )
+
 print("*** after filling ***")
-# [print(f"{all_hists[i]}\n") for i in range(2)]
-
-# Plot hists
-# fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
-# for leave, ax, h in zip(all_leaves, axes.flatten(), all_hists):
-#     data_leaf = df[leave]
-#
-#     h[:, :, 9j, 2j].plot2d(ax=ax, label=f"{leave}", ls="solid")
-#
-# plt.show()
-
 
 volume_layer_pairs = [(9, 2), (9, 4), (16, 2), (16, 4), (16, 6)]
 for v_l_pair in volume_layer_pairs:
@@ -125,20 +129,32 @@ for v_l_pair in volume_layer_pairs:
     for leave, ax, h in zip(all_leaves, axes.flatten(), all_hists):
         data_leaf = df[leave]
 
-        # # TODO how should we handle the surfaces?
-        # v_l_pair = (9, 2)
-        # log_volume = volume_id == v_l_pair[0]
-        # log_layer = layer_id == v_l_pair[1]
-        # log_plot = log_volume * log_layer
-        # fig, ax = plt.subplots()
-        # all_hists[leave_i].fill(ak.flatten(data_leaf[log_plot]), ak.flatten(data_eta[log_plot]))
-        # all_hists[leave_i].fill(ak.flatten(data_leaf), ak.flatten(data_eta))
-
         h[:, :, v_id * 1j, l_id * 1j].plot2d(ax=ax, label=f"{leave}", ls="solid")
 
+        density = h[:, :, v_id * 1j, l_id * 1j].density()
+
+        bins = h[:, :, v_id * 1j, l_id * 1j].axes[0].edges
+        bins = bins[0:-1]
+        bins += (bins[1] - bins[0]) / 2
+
+        means_all = []
+        rms_all = []
+        for density_row in density.T:
+            m = sum(density_row * bins) / sum(density_row)
+            rms = np.sqrt(sum(density_row * (bins - m) ** 2) / sum(density_row))
+
+            means_all.append(m)
+            rms_all.append(rms)
+
+        v_space = -0.285
+        for i, (m, rms) in enumerate(zip(means_all, rms_all)):
+            s = f"$\\mu$ = {m:.3f}, $\\sigma$ = {rms:.3f}"
+            x_pos = -6
+            y_pos = 1.35 + i * v_space
+            plt.text(x_pos, y_pos, s, fontsize=8, color="w")
+
     plt.suptitle(f"Volume {v_id}, Layer {l_id}", fontsize=14)
-    # plt.show()
-    plt.savefig(f"res_itk_vol{v_id}_lay{l_id}.pdf")
+    plt.savefig(f"pull_itk_vol{v_id}_lay{l_id}.pdf")
 
 
 # ITk Geometry
