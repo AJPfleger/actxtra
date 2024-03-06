4074aa2195dd0f7b55b5d4ef89b66ac0afc8ae97
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-03-06 10:53:10 +0100

    improve hist plotter

--- scripts/hist_of_measurements.py
+++ scripts/hist_of_measurements.py
@@ -1,34 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def flatten2(list_of_list_of_lists):
+    step1 = [x for xs in list_of_list_of_lists for x in xs]
+    return [x for xs in step1 for x in xs]
+
+
+energies = [1, 10, 100]
+
 # Read data
-meas_in = []
-meas_out = []
+meas_io_all = []
+for e in energies:
+    filename = f"../../gx2f-push/testexports/tmp999_tests{e}"
+    meas_in = []
+    meas_out = []
+    with open(filename, "r") as file:
+        for line in file:
+            if line.startswith("QPDATAINPUTMEASUREMENTSSIZE"):
+                parts = line.split()
+                meas_in.append(int(parts[1]))
+            elif line.startswith("QPDATAOUTMEASUREMENTSSIZE"):
+                parts = line.split()
+                meas_out.append(int(parts[1]))
+    # meas_out = meas_out * 3
+    meas_io_all.append([meas_in, meas_out])
 
-filename = "../../gx2f-push/testexports/tmp999_tests"
-with open(filename, "r") as file:
-    for line in file:
-        if line.startswith("QPDATAINPUTMEASUREMENTSSIZE"):
-            parts = line.split()
-            meas_in.append(int(parts[1]))
-        elif line.startswith("QPDATAOUTMEASUREMENTSSIZE"):
-            parts = line.split()
-            meas_out.append(int(parts[1]))
-
-# Plot the histogram
-x = [meas_in, meas_out]
-bins = range(np.min(np.concatenate(x)), np.max(np.concatenate(x)) + 2)
-
-left_of_first_bin = np.min(np.concatenate(x)) - 1.0 / 2
-right_of_last_bin = np.max(np.concatenate(x)) + 1.0 / 2
+# Prepare histogram binning
+left_of_first_bin = min(flatten2(meas_io_all)) - 1.0 / 2
+right_of_last_bin = max(flatten2(meas_io_all)) + 1.0 / 2
 bins = np.arange(left_of_first_bin, right_of_last_bin + 1, 1)
 
+# Plot histograms
+fig, axs = plt.subplots(1, len(energies), figsize=(15, 5))
 
-fig, ax = plt.subplots()
-ax.hist(x, bins=bins, histtype="bar", label=["before fit", "after fit"])
-ax.set_title("Histogram of Measurements in the GX2F")
-ax.set_xlabel("Number of measurements")
-ax.legend(prop={"size": 10})
+for i, meas_io in enumerate(meas_io_all):
+    ax = axs[i]
+    ax.hist(meas_io, bins=bins, histtype="bar", label=["before fit", "after fit"])
+    ax.set_title(f"Histogram of Measurements for {energies[i]} GeV")
+    ax.set_xlabel("Number of measurements")
+    ax.legend(prop={"size": 10})
 
-
-plt.show()
+plt.tight_layout()
+# plt.show()
+plt.savefig("hist_of_measurements_100k_events.png")
